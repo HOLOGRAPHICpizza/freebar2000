8b014e52e3abd3723a4b5816b6101bfad043731d
Michael Craft <mcraft@peak15.org>
2013-01-17 13:02:36 -0500

    Initial commit.

---
+++ WordEditorCore/build/classes-generated/org/peak15/freebar2000/word/editor/core/Bundle.java
@@ -0,0 +1,27 @@
+package org.peak15.freebar2000.word.editor.core;
+/** Localizable strings for {@link org.peak15.freebar2000.word.editor.core}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Word</i>
+     * @see WordTopComponent
+     */
+    static String CTL_WordAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_WordAction");
+    }
+    /**
+     * @return <i>Word Window</i>
+     * @see WordTopComponent
+     */
+    static String CTL_WordTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_WordTopComponent");
+    }
+    /**
+     * @return <i>This is a Word window</i>
+     * @see WordTopComponent
+     */
+    static String HINT_WordTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_WordTopComponent");
+    }
+    private void Bundle() {}
+}
